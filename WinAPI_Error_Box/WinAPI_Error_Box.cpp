fdad1834b11d49e51b950cf8d8f0a8d01e3455c4
Oliver Martens <o.martens2001@gmail.com>
2023-10-26 00:17:29 +0200

    Hello WorldBox

--- WinAPI_Error_Box/WinAPI_Error_Box.cpp
+++ WinAPI_Error_Box/WinAPI_Error_Box.cpp
@@ -3,7 +3,8 @@
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     PSTR lpCmdLine, int nCmdShow)
 {
-    MessageBox(0,L"Fatal Error!", L"Error",MB_OKCANCEL | MB_ICONERROR);
-    MessageBox(0,L"Fatal Error!", L"Error",MB_OKCANCEL | MB_ICONERROR);
+    //MessageBox(0,L"Fatal Error!", L"Error",MB_OKCANCEL | MB_ICONERROR);
+    //MessageBox(0,L"Fatal Error!", L"Error",MB_OKCANCEL | MB_ICONERROR);
+    MessageBoxA(0, "Hello World", "Text", MB_DEFBUTTON1);
     return 0;
 }
