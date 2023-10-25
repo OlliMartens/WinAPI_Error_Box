24f0400a4e83f0aba8b29687ab1e3817e63d46d5
Oliver Martens <o.martens2001@gmail.com>
2023-10-26 00:14:03 +0200

    Erste kleine Error Messagebox

--- WinAPI_Error_Box/WinAPI_Error_Box.cpp
+++ WinAPI_Error_Box/WinAPI_Error_Box.cpp
@@ -3,6 +3,7 @@
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     PSTR lpCmdLine, int nCmdShow)
 {
-    MessageBox(0,"Fatal Error!");
+    MessageBox(0,L"Fatal Error!", L"Error",MB_OKCANCEL | MB_ICONERROR);
+    MessageBox(0,L"Fatal Error!", L"Error",MB_OKCANCEL | MB_ICONERROR);
     return 0;
 }
